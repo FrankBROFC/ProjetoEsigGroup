def951902bd688341856916753db8ff58e602e9b
Francisco Filho <fcofilho0809@gmail.com>
2022-06-08 17:03:22 -0300

    fix: Remoção dos ENUMS

--- gerenciadorTarefasAPI/src/main/java/com/frank/gerenciadorTarefasAPI/enums/Prioridade.java
+++
@@ -1,7 +0,0 @@
-package com.frank.gerenciadorTarefasAPI.enums;
-
-public enum Prioridade {
-    ALTA,
-    MEDIA,
-    BAIXA
-}
--- gerenciadorTarefasAPI/src/main/java/com/frank/gerenciadorTarefasAPI/enums/Situacao.java
+++
@@ -1,7 +0,0 @@
-package com.frank.gerenciadorTarefasAPI.enums;
-
-public enum Situacao {
-    PENDENTE,
-    CONCLUIDO,
-
-}
